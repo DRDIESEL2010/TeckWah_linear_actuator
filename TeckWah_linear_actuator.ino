abe7d6494eeb811fc95069d2d4ebdc0f3336ae33
Benjamin Low <lthben@gmail.com>
2015-11-18 13:22:55 +0800

    supports two motors instead of one

--- TeckWah_linear_actuator.ino
+++ TeckWah_linear_actuator.ino
@@ -1,29 +1,24 @@
 /*
  * Author: Benjamin Low
- * Last updated: 5 Oct 2015
- * Description: Controls a linear actuator using yfrobot motor shield.
+ * Last updated: 18 Nov 2015
+ * Description: Controls up to two linear actuators using yfrobot motor shield.
  * Documentation online is similar to dfrobot's shield.
  * http://www.dfrobot.com/index.php?route=product/product&product_id=69#.VYy9oe2qqko
  */
-int motor1_pwmPin = 5;
-int motor1_dirPin = 4;
-//int motor2_pwmPin = 6;
-//int motor2_dirPin = 7;
-
-int switchForwardPin = 8;
-int switchBackwardPin = 9;
-int potPin = A0;
+int motor1_pwmPin = 5; //speed
+int motor1_dirPin = 4; //direction
+int motor2_pwmPin = 6;
+int motor2_dirPin = 7;
 
 void setup() {
   pinMode(motor1_pwmPin, OUTPUT);
-  //  pinMode(motor2_pwmPin, OUTPUT);
   pinMode(motor1_dirPin, OUTPUT);
-  //  pinMode(motor2_dirPin, OUTPUT);
-  pinMode(switchForwardPin, INPUT);
-  pinMode(switchBackwardPin, INPUT);
-  pinMode(potPin, INPUT);
+  pinMode(motor2_pwmPin, OUTPUT);
+  pinMode(motor2_dirPin, OUTPUT);
+  
   Serial.begin(9600);
   analogWrite(motor1_pwmPin, 0);
+  analogWrite(motor2_pwmPin, 0);
 }
 
 void loop() {
@@ -31,6 +26,7 @@ void loop() {
   unsigned char incomingbyte = 0;
 
   if (Serial.available() > 0) {
+        
     incomingbyte = Serial.read();
 
     if (incomingbyte == '0') {
@@ -40,39 +36,18 @@ void loop() {
     } else if (incomingbyte == '1') {
       
       analogWrite(motor1_pwmPin, 255); //max speed
-      digitalWrite(motor1_dirPin, HIGH);
+      digitalWrite(motor1_dirPin, HIGH); //extend
       
     } else if (incomingbyte == '2') {
       
       analogWrite(motor1_pwmPin, 255); //max speed
-      digitalWrite(motor1_dirPin, LOW);
+      digitalWrite(motor1_dirPin, LOW); //retract
     }
   }
-
-  //ben_test();
-
-  delay(30);
-}
-
-void ben_test() {
   
-  int speedValue = analogRead(potPin) / 4;
-        
-  if ( (digitalRead(switchForwardPin) == LOW) && (digitalRead(switchBackwardPin) == LOW) ) {
-    //        Serial.println("low");
-    analogWrite(motor1_pwmPin, 0);
-  }
-
-  else { //one of the buttons is pressed
-    //write the speed
-    analogWrite(motor1_pwmPin, speedValue);
-    //which direction?
-    if ( digitalRead(switchForwardPin) == HIGH ) {
-      digitalWrite(motor1_dirPin, HIGH);
-    } else if (digitalRead(switchBackwardPin) == HIGH) {
-      digitalWrite(motor1_dirPin, LOW);
-    }
-  }
+  delay(20);
 }
+
+
 
 
